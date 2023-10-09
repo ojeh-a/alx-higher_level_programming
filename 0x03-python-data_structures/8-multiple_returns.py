911a02909680f45581bb326c9269a26690faf920
Ojeh <ojehdayo@gmail.com>
2023-10-09 16:42:04 +0100

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence == ():
+    if sentence == "":
         first = None
     else:
         first = sentence[0]
