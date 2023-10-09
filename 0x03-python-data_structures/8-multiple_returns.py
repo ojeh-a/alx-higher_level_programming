d71dd32a22bdf040894eff3bdc45f7f44c1533e3
Ojeh <ojehdayo@gmail.com>
2023-10-09 16:23:09 +0100

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return (None)
     first = sentence[0]
     length = len(sentence)
+    if sentence is None:
+        first = None
     return (length, first)
