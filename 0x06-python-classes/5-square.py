3b2b63b4ba39aecf59574bc1c85a8e7d7878a5ab
Ojeh <ojehdayo@gmail.com>
2023-10-30 09:15:27 +0100

    Accessing and updating private attribute

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Define a class Square. """
+
+
+class Square:
+    """Represent a square. """
+
+    def __init__(self, size=0):
+        """
+        Initialize a square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Get/set the current size of the square. """
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size__ = value
+
+    def area(self):
+        """ Return the current area of the square. """
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ Print size of square with '#' """
+
+        if self.__size__ == 0:
+            print()
+        else:
+            for i in range(self.__size__):
+                print("#" * self.__size__)
