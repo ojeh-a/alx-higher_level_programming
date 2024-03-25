16c549b04a993d3960948fa24de8b684a4e71b62
Ojeh <ojehdayo@gmail.com>
2024-03-25 12:38:57 +0100

    rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && typeof w === 'number' && typeof h === 'number') {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let eof = '';
+      for (let j = 0; j < this.width; j++) {
+        eof += 'X';
+      }
+      console.log(eof);
+    }
+  }
+}
+module.exports = Rectangle;
