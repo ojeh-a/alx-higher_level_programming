aba81681c901268b7c7e4ac4dee7593825ebc223
Ojeh <ojehdayo@gmail.com>
2024-04-01 23:24:07 +0100

    connect'

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import MySQLdb
+'''
+This script connects to mysql and lists all states from
+the database `hbtn_0e_usa`
+'''
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                        database=sys.argv[3])
+
+    # getting a cursor
+    cur = db.cursor()
+
+    # query to select states by id
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    #print
+    [print(state) for state in cur.fetchall()]
