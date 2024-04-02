1b433b25ed2233ad1c9293c978b6a1d4d28a03c7
Ojeh <ojehdayo@gmail.com>
2024-04-02 20:39:25 +0100

    shebang

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 '''
 This script connects to mysql and lists all states from
 the database `hbtn_0e_usa`
 '''
+import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
