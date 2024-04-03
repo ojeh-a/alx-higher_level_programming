2f3fb62fb3826a3ed83ac92a9e51137052461ba7
Ojeh <ojehdayo@gmail.com>
2024-04-03 01:11:01 +0100

    search

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes in an argument and displays all values in
+    the table that matches the argument.
+usage:
+    ./2-my_filter_states.py <user> <password> <db> <search_name>
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER
+                BY id".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
