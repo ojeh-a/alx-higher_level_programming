7787789c8ecd200451b9eb392bec0deba7221ed3
Ojeh <ojehdayo@gmail.com>
2024-04-03 20:30:42 +0100

    removed ASC

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id".
                 format(sys.argv[4]))
     [print(state) for state in cur.fetchall()]
