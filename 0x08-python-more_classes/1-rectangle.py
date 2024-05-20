eb6528978062a7cb7ba10ccc794ed7e4eecf065e
Ojeh <ojehdayo@gmail.com>
2024-05-20 11:48:50 +0100

    1-rectangle fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -43,6 +43,6 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must >= 0")
         self.__height = value
