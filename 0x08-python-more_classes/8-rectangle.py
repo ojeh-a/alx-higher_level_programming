3a89f7a3e9dda2bbedd378cc4dc3ff63618a6df4
Ojeh <ojehdayo@gmail.com>
2024-05-21 23:44:47 +0100

    Square

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
 
         if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.width * self.height)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         """Reutrns string representation of symblo"""
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,3 +81,18 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Compare Rectangles"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
