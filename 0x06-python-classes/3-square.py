6992585164b77c75eaf3600514f73637da5534dc
Ojeh <ojehdayo@gmail.com>
2025-08-24 09:38:51 +0100

    Access and update private attribute

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Instantiates a Square with size
+        Args: size(int)
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the square a a square Instant"""
+        self.area = self.__size ** 2
+        return self.area
